87adba114d65a8fd6cd58c32b7004cd26896d6e2
Dan LaManna <me@danlamanna.com>
2020-01-21 21:54:42 -0500

    Add base code for addon

---
+++ GimmeTheLoot.lua
@@ -0,0 +1,27 @@
+if not _TEST then
+  GimmeTheLoot = LibStub("AceAddon-3.0"):NewAddon("GimmeTheLoot")
+end
+
+local defaults = {
+  profile = {
+    rolls = {}
+  }
+}
+
+local function ItemIdFromRoll(rollID)
+  -- TODO: implement
+  return rollID
+end
+
+function GimmeTheLoot:TrackLootRoll(rollID, rollTime, lootHandle) -- luacheck: no unused args
+  table.insert(self.db.profile.rolls, {
+                 time = rollTime,
+                 itemId = ItemIdFromRoll(rollID)
+  })
+end
+
+function GimmeTheLoot:OnInitialize()
+  -- TODO: use self vs GimmeTheLoot syntax
+  self.db = LibStub("AceDB-3.0"):New("GTL_DB", defaults)
+  self:RegisterEvent("START_LOOT_ROLL", "TrackLootRoll")
+end
