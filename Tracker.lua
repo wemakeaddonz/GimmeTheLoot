27bc2ce175d3013de927d4faa8b16dd13203702e
mike <ubermikeleet@gmail.com>
2020-02-03 02:15:42 -0500

    Implemented Tracker functionality

--- Tracker.lua
+++ Tracker.lua
@@ -2,14 +2,22 @@ if not _TEST then
     Tracker = GimmeTheLoot:NewModule('Tracker', 'AceEvent-3.0')
 end
 
-activeRollCounter = 0
-activeRollTotal = 0
+local LootHistoryGetItem = C_LootHistory.GetItem
+local LootHistoryGetPlayerInfo = C_LootHistory.GetPlayerInfo
 
+-- register loot events
 function Tracker:OnEnable()
+    local activeRollCounter = 0
+    local activeRollTotal = 0
+
+    -- increase counter whenever a loot roll starts
     self:RegisterEvent('START_LOOT_ROLL', function()
         activeRollCounter = activeRollCounter + 1
         activeRollTotal = activeRollTotal + 1
     end)
+
+    -- decrease counter whenever a loot roll ends
+    -- if counter == 0, all active loot rolls have ended, so call AddRolls and reset total
     self:RegisterEvent('LOOT_ROLLS_COMPLETE', function()
         activeRollCounter = activeRollCounter - 1
         if activeRollCounter == 0 then
@@ -21,4 +29,23 @@ end
 
 -- add loot rolls to database
 function Tracker:AddRolls(numRolls)
+    for i = 1, numRolls do
+        local record = {item = {}, rolls = {}}
+        local _, itemLink, numPlayers = LootHistoryGetItem(i)
+        local itemName, _, itemQuality = GetItemInfo(itemLink)
+
+        record.item.link = itemLink
+        record.item.name = itemName
+        record.item.quality = itemQuality
+
+        for p = 1, numPlayers do
+            local playerName, _, rollType, rollValue, isWinner = LootHistoryGetPlayerInfo(i, p)
+
+            table.insert(record.rolls, {name = playerName, type = rollType, roll = rollValue})
+            record.winner = isWinner and playerName
+        end
+    end
+
+    record.rollCompleted = time()
+    table.insert(GimmeTheLoot.db.profile.records, record) -- needs test
 end
